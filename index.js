daedd9f161ec908212d848ea15ae4598525f9151
unknown <amitbiswas9282@gmail.com>
2024-07-02 19:49:38 +0600

    This is Amit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 <p>This is index.js</p>;
 <p>This is index.js</p>;
 <p>It looks like you're trying to commit changes to a Git repository</p>;
+<p>This line is added bY Amit</p>;
+<p>This line is added bY Amit Biswas 2</p>;
+<p>This line is added bY Amit Biswas 2</p>;
